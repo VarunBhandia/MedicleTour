a31144098e8be28fed7d340fb8e7559cf48e770b
Varun Bhandia <varunbhandia@gmail.com>
2018-12-17 23:17:45 +0530

    Database

--- database/medicle.sql
+++ database/medicle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2018 at 06:37 AM
+-- Generation Time: Dec 17, 2018 at 06:47 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -38,7 +38,10 @@ CREATE TABLE `countries` (
 --
 
 INSERT INTO `countries` (`couid`, `couname`) VALUES
-(1, 'India');
+(1, 'India'),
+(2, 'Malaysia'),
+(3, 'Singapore'),
+(4, 'Thailand');
 
 -- --------------------------------------------------------
 
@@ -49,16 +52,8 @@ INSERT INTO `countries` (`couid`, `couname`) VALUES
 CREATE TABLE `diseases` (
   `did` int(11) NOT NULL,
   `dname` varchar(255) NOT NULL,
-  `dlink` varchar(2550) NOT NULL
+  `dlink` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `diseases`
---
-
-INSERT INTO `diseases` (`did`, `dname`, `dlink`) VALUES
-(1, 'asthama', 'www.facebook.com'),
-(2, 'Brain cancer', 'google.com');
 
 -- --------------------------------------------------------
 
@@ -68,16 +63,17 @@ INSERT INTO `diseases` (`did`, `dname`, `dlink`) VALUES
 
 CREATE TABLE `doctors` (
   `docid` int(11) NOT NULL,
-  `docname` varchar(255) NOT NULL
+  `docname` varchar(255) NOT NULL,
+  `docspec` varchar(255) NOT NULL,
+  `docdesig` varchar(255) NOT NULL,
+  `dochosp` varchar(255) NOT NULL,
+  `doccou` varchar(255) NOT NULL,
+  `docsd` varchar(255) NOT NULL,
+  `docld` varchar(2550) NOT NULL,
+  `docurl` varchar(255) NOT NULL,
+  `docmetatitle` varchar(255) NOT NULL,
+  `docmetadesc` varchar(2550) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `doctors`
---
-
-INSERT INTO `doctors` (`docid`, `docname`) VALUES
-(3, 'varun'),
-(4, 'naresh');
 
 -- --------------------------------------------------------
 
@@ -87,16 +83,16 @@ INSERT INTO `doctors` (`docid`, `docname`) VALUES
 
 CREATE TABLE `hospitals` (
   `hid` int(11) NOT NULL,
-  `hname` varchar(255) NOT NULL
+  `hname` varchar(255) NOT NULL,
+  `hospspec` varchar(255) NOT NULL,
+  `hospcou` varchar(255) NOT NULL,
+  `hospsd` varchar(255) NOT NULL,
+  `hospld` varchar(2550) NOT NULL,
+  `hospurl` varchar(255) NOT NULL,
+  `hospmetatitle` varchar(255) NOT NULL,
+  `hospmetadesc` varchar(255) NOT NULL,
+  `hospimg` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `hospitals`
---
-
-INSERT INTO `hospitals` (`hid`, `hname`) VALUES
-(1, 'fortis'),
-(2, 'appollo');
 
 -- --------------------------------------------------------
 
@@ -159,22 +155,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `countries`
 --
 ALTER TABLE `countries`
-  MODIFY `couid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `couid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `diseases`
 --
 ALTER TABLE `diseases`
-  MODIFY `did` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `did` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `hospitals`
 --
 ALTER TABLE `hospitals`
-  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
